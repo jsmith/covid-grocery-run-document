73698e591afaae1697064b38a9b3421d010fba41
Jacob Smith <jacob.smith@unb.ca>
2020-07-13 12:25:21 -0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Design Document
+The COVID Shopping design document.
+
+## Setup
+```
+npm i
+```
+
+## Usage
+```
+npx tailwindcss build src/index.css -o src/tailwind.css
+prince src/index.html # or npx nodemon
+```
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  theme: {
+    screens: {}
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    gridRow: false,
+    gridRowStart: false,
+    gridRowEnd: false,
+    gap: false,
+    placeholderOpacity: false,
+    placeholderColor: false,
+    gridAutoFlow: false,
+    gridColumn: false,
+    gridColumnStart: false,
+    gridColumnEnd: false,
+    gridTemplateColumns: false,
+    gridTemplateRows: false,
+    overflow: false,
+    pointerEvents: false,
+    resize: false,
+    space: false,
+    divideWidth: false,
+  },
+  purge: ["./src/**/*.html"],
+};
